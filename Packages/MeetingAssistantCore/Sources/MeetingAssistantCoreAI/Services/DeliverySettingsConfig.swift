4af536dcf2d89b08f6d72233add61d897e230a77
Renato <remourato@gmail.com>
2026-02-11 17:19:27 -0300

    chore(settings): remove meeting delivery keys

--- Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreAI/Services/DeliverySettingsConfig.swift
+++ Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreAI/Services/DeliverySettingsConfig.swift
@@ -6,8 +6,6 @@ import MeetingAssistantCoreInfrastructure
 public protocol DeliverySettingsConfig {
     var autoCopyTranscriptionToClipboard: Bool { get }
     var autoPasteTranscriptionToActiveApp: Bool { get }
-    var meetingAutoCopyTranscriptionToClipboard: Bool { get }
-    var meetingAutoPasteTranscriptionToActiveApp: Bool { get }
 }
 
 /// Extend existing AppSettingsStore to conform to the protocol directly.
