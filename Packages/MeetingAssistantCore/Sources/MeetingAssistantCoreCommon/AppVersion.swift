1e116145d1b20b887f6e644652e289ebb8e93968
Renato <renato.mourato@nzn.io>
2026-02-12 16:30:37 -0300

    chore: update versioning to 0.2.0 and add bump-version script - Updated CFBundleShortVersionString and CFBundleVersion in Info.plist files. - Modified AppVersion.swift to reflect new version and build numbers. - Introduced bump-version.sh script for automated version updates.

--- Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
+++ Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
@@ -14,10 +14,10 @@ public enum AppVersion {
     // MARK: - Hardcoded Constants (Update these when releasing)
 
     /// Hardcoded version string - update this when releasing a new version
-    private static let hardcodedVersion = "0.1.1"
+    private static let hardcodedVersion = "0.2.0"
 
     /// Hardcoded build number - update this when creating a new build
-    private static let hardcodedBuild = "1"
+    private static let hardcodedBuild = "20"
 
     // MARK: - Public API
 
