d680b255275a612ac94514226cc6b8c58c8bc794
Renato <renato.mourato@nzn.io>
2026-02-12 22:37:44 -0300

    chore: update app version to 0.2.2 and build number to 22 in Info.plist and AppVersion.swift

--- Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
+++ Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
@@ -14,10 +14,10 @@ public enum AppVersion {
     // MARK: - Hardcoded Constants (Update these when releasing)
 
     /// Hardcoded version string - update this when releasing a new version
-    private static let hardcodedVersion = "0.2.0"
+    private static let hardcodedVersion = "0.2.2"
 
     /// Hardcoded build number - update this when creating a new build
-    private static let hardcodedBuild = "20"
+    private static let hardcodedBuild = "22"
 
     // MARK: - Public API
 
