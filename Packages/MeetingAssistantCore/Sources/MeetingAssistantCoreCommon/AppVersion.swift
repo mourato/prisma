3464a823d7fd03bd8689a31b96cbd2b8659992c0
Renato <remourato@gmail.com>
2026-03-03 14:23:54 -0300

    feat(tests): add heartbeat progress tracking and test count extraction for Xcode tests

--- Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
+++ Packages/MeetingAssistantCore/Sources/MeetingAssistantCoreCommon/AppVersion.swift
@@ -14,10 +14,10 @@ public enum AppVersion {
     // MARK: - Hardcoded Constants (Update these when releasing)
 
     /// Hardcoded version string - update this when releasing a new version
-    private static let hardcodedVersion = "0.2.28"
+    private static let hardcodedVersion = "0.3.3"
 
     /// Hardcoded build number - update this when creating a new build
-    private static let hardcodedBuild = "24"
+    private static let hardcodedBuild = "25"
 
     // MARK: - Public API
 
